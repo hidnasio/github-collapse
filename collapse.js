b5eb90ddc8e340cd0dad2059957d3767c51be4cb
Luis Ferreira <hidnasio@gmail.com>
2018-06-12 09:04:16 -0300

    Removes  elements after collapse

--- collapse.js
+++ collapse.js
@@ -1,7 +1,11 @@
 const file_collapse_button = '#files .file button[aria-expanded]';
+const touch_divs = '.modal-backdrop.js-touch-events';
 
 browser.runtime.onMessage.addListener(doit);
 
 function doit(request, sender, sendReponse) {
   document.querySelectorAll(file_collapse_button).forEach(b => b.click());
+  document.querySelectorAll(touch_divs).forEach(
+    b => b.parentNode.removeChild(b)
+  );
 }
